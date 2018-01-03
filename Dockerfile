4db06c23646eb098c4615b18d8a30fdf8877ad4f
Mav Peri <mp@h2labs.co.uk>
2018-01-03 15:05:00 +1100

    migrations

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
             git \
             # for curl https
             apt-transport-https \
+            vim \
     && docker-php-ext-install -j$(nproc) iconv mcrypt \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install -j$(nproc) gd \
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -79,17 +79,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    table tennis collection
+                    table tennis stats
                 </div>
-<!--
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-                -->
+                <id>A basic table tennis results data collection and reporting app</id>
             </div>
         </div>
     </body>
